d84737ba03c193da5ae1be54ca99f2527f820760
Sonia Marshall <soniammarshall@live.co.uk>
2020-02-22 16:03:36 +0000

    add camera image

--- webapp/app.py
+++ webapp/app.py
@@ -4,6 +4,8 @@ sys.path.insert(1, '../grove.py/grove/')
 from flask import Flask, render_template
 import client
 import grove_moisture_sensor as grove
+import photo
+import time
 
 app = Flask(__name__)
 sensor_reading=0
@@ -17,11 +19,14 @@ def stats():
     return render_template('stats.html')
 
 @app.route('/move<direction>')
+#update direction to be 23 for (2,3)
 def move(direction):
     if (direction == 'forward'):
         message = "Move Motors"
+    elif (direction == 'backward'):
+        message="Move Motors Backward"
     else:
-        message="Move Motors Backwards"
+        message = direction
     client.initClient(message)
     return render_template('index2.html', sensor_reading="Please Click Button")
 
@@ -29,8 +34,14 @@ def move(direction):
 def sensor_reading():
     return render_template('index2.html', sensor_reading=grove.sensor_readings())
 
+@app.route('/image')
+def image():
+    photo.clickPhoto()
+    time.sleep(1)
+    return render_template('image.html')
 
 if __name__ == '__main__':
 #    app.run(debug=True, host='127.0.1.1',port=8080)
     app.run(host='0.0.0.0', debug=True)
 #    app.run()
+
