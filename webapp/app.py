e71cf3b41535de902a60b957b1924b96cec30372
soniammarshall <soniammarshall@live.co.uk>
2020-02-22 20:17:42 +0000

    committing to python 3

--- webapp/app.py
+++ webapp/app.py
@@ -4,11 +4,11 @@ sys.path.insert(1, '../grove.py/grove/')
 from flask import Flask, render_template, jsonify
 import client
 import grove_moisture_sensor as grove
-#import photo
+import photo
 import time
-from timeloop import Timeloop
-from datetime import timedelta
-tl = Timeloop()
+#from timeloop import Timeloop
+#from datetime import timedelta
+#tl = Timeloop()
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -43,17 +43,17 @@ def sensor_reading():
     
 #return render_template('overrides.html', sensor_reading="Soil Moisture: 327, Temperature: 22C")
 
-#@app.route('/image')
-#def image():
-#   photo.clickPhoto()
-#    time.sleep(1)
-#    return render_template('image.html')
+@app.route('/image')
+def image():
+    photo.clickPhoto()
+    time.sleep(1)
+    return render_template('image.html')
 
-@tl.job(interval=timedelta(seconds=60))
-def sample_job_every_60s():
-    print "60s : {}".format(time.ctime())
+#@tl.job(interval=timedelta(seconds=60))
+#def sample_job_every_60s():
+#    print "60s : {}".format(time.ctime())
 
 if __name__ == '__main__':
-    tl.start(block=False)
+    #tl.start(block=False)
     # If debug is set to true, another timeloop instance is started for some reason
     app.run(host='0.0.0.0', debug=False)
