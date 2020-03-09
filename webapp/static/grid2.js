daf02f57fc4d9a0b178e0bc7da0b40c33b447852
dmitreeeee <d.boettcher@live.com>
2020-03-09 20:38:10 +0000

    Added different views and plant add feature

--- webapp/static/grid2.js
+++ webapp/static/grid2.js
@@ -1,6 +1,6 @@
 
 
-const maxRow = 20;
+const maxRow = 10;
 const maxCol = 15;
 
 function classList(classes) {
@@ -11,19 +11,63 @@ function classList(classes) {
     .join(' ');
 }
 
-function Plot(props) {
-  var liClasses = classList({
-   'plot': true,
-   'plantable': props.plantable
- });
-  return (
-   <button className={liClasses} onClick={props.onClick}>
-    {props.icon}
-    </button>
-  );
+function indexToGrid(index){
+  var row = Math.floor(index / maxCol);
+  var col = index - row*maxCol;
+  console.log(`${row},${col} <== ${index}`)
+  return [row,col];
 }
 
+function gridToIndex(row,col){
+  var index = row * maxCol + col;
+  console.log(`${row},${col} ==> ${index}`)
+  if (row < 0 || col < 0|| row >= maxRow|| col >= maxCol){
+    return -1;
+  }
+  return index;
+}
+
+
 class Board extends React.Component {
+
+  renderPlot(i) {
+
+    var icon = this.state.icons[i];
+    var plantable = this.state.plantable[i];
+    var mode = this.state.mode;
+
+    if (mode == "admin"){
+      if (plantable){
+        icon = '➕';
+      }
+      if (this.state.actionArray.has(i)){
+        icon = 'ℹ️';
+      }
+    } else if (mode == "user"){
+      plantable = true; //disables background override
+      if (this.state.actionArray.has(i)){
+        icon = 'ℹ️';
+      }
+    } else if (mode == "water"){
+      if (this.state.actionArray.has(i)){
+        icon = '💧';
+      }
+    }
+
+
+
+    var liClasses = classList({
+      'plot': true,
+      'plantable': plantable
+    });
+
+
+    return (
+      <button className={liClasses} onClick={() => this.handleClick(i)}>
+      {icon}
+      </button>
+    );
+  }
 
 
 
@@ -32,43 +76,10 @@ class Board extends React.Component {
     this.state = {
       icons: Array(maxCol*maxRow).fill(null),
       plantable: Array(maxCol*maxRow).fill(true),
+      mode: "user",
+      output: "",
+      actionArray: new Set()
     };
-
-  }
-
-
-  handleClick(i) {
-    const icons = this.state.icons.slice();
-    icons[i] = '💧';
-    this.setState({
-      icons: icons,
-    });
-  }
-
-  renderPlot(i) {
-    return (
-      <Plot
-        onClick={() => this.handleClick(i)}
-        icon={this.state.icons[i]}
-        plantable={this.state.plantable[i]}
-      />
-    );
-  }
-
-markUnPlantable(centre,radius){
-  var row = centre % maxCol;
-  var col = centre - row*maxCol;
-
-  for (var i = row - radius; i <= row + radius; i++){
-    for (var j = col - radius; j<= col + radius; j++) {
-      this.state.plantable[i*maxCol + j ] = false;
-    }
-  }
-}
-
-render(){
-    const workspace = "Appleton 3";
-    const rows = [];
 
     for (var key in plotsJson['plants']){
       var plot = plotsJson['plants'][key]['plot']
@@ -77,13 +88,84 @@ render(){
       this.markUnPlantable(plot,plotsJson['plants'][key]['size']);
     }
 
-    for (var a in this.state.plantable){
-      if (this.state.plantable[a]){
-        this.state.icons[a] = '➕'
+
+  }
+
+
+
+  handleClick(i) {
+    const icons = this.state.icons.slice();
+    var actionArray = this.state.actionArray;
+
+    if (this.state.mode == "water" && this.state.icons[i]){
+      //icons[i] = '💧';
+
+      if (this.state.actionArray.has(i)){
+        actionArray.delete(i);
+      } else {
+        actionArray.add(i);
+      }
+      console.log(actionArray);
+    }
+
+    if (this.state.mode != "water" && this.state.icons[i]){
+      if (this.state.actionArray.has(i)){
+        actionArray = new Set();
+      } else {
+        actionArray = new Set();
+        actionArray.add(i);
       }
     }
 
+    if (this.state.mode == "admin" && this.state.plantable[i]){
+      icons[i] = '👨‍🌾';
+      console.log(document.getElementById('size').value);
+      this.markUnPlantable(i,parseInt(document.getElementById('size').value));
+    }
 
+
+    this.setState({
+      icons: icons,
+      output: `Clicked at ${i} ==> ${indexToGrid(i)}`,
+      actionArray: actionArray
+    });
+  }
+
+actionSelected(){
+  console.log(Array.from(this.state.actionArray).join(' '))
+
+  //this.state.output = actionArray;
+    this.setState({
+      output: Array.from(this.state.actionArray).map(indexToGrid).join(' & '),
+      actionArray: new Set()
+    })
+}
+
+
+
+markUnPlantable(centre,radius){
+
+  var [row,col] = indexToGrid(centre);
+
+  for (var i = row - radius; i <= row + radius; i++){
+    for (var j = col - radius; j<= col + radius; j++) {
+      this.state.plantable[gridToIndex(i,j)] = false;
+    }
+  }
+}
+
+
+changeMode(){
+
+  this.setState({
+    mode: document.getElementById('mode').value,
+    actionArray: new Set()
+  })
+}
+
+render(){
+    const workspace = "Appleton 3";
+    const rows = [];
 
     for (var y = 0; y<maxRow;y++){
       const row = [];
@@ -93,11 +175,26 @@ render(){
       rows.push(  <div className="plant-row">{row}</div>)
     }
 
+    var action = (this.state.mode == "admin")? "Remove" : "Water";
 
     return (
       <div>
         <div className="workspace">{workspace}</div>
+        <select id="mode" onChange={() => this.changeMode()}>
+          <option value="user">User</option>
+          <option value="water">Water</option>
+          <option value="admin">Admin</option>
+        </select>
+        <select id="size">
+          <option value="0">r = 0</option>
+          <option value="1">r = 1</option>
+          <option value="2">r = 2</option>
+          <option value="3">r = 3</option>
+        </select>
+        <div className="mode">View = {this.state.mode}</div>
         {rows}
+        <button className="waterButton" onClick={() => this.actionSelected()}>{action}</button>
+        <div className="message">Output = {this.state.output}</div>
       </div>
     );
 }
